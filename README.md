ee97d1790ce35cc241fddfb2ae2a64aec64a5059
Robert Starosta <49361427+SonicSong@users.noreply.github.com>
2021-08-14 01:10:39 +0100

    first and final commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# jokeproject
+
+This is a simple python window project to learn about usage of tkinter.
+
+It works also as precaution about not writing credit card data into any type software or website that might ask about it.
+
+If anyone wants to use it as example than sure go on. BUT DO NOT TURN IT INTO ACTUAL MALWARE!
+
+Also name of the character is Tsukasa Hiiragi and she is from Lucky Star anime.
---
+++ main.py
@@ -0,0 +1,53 @@
+from tkinter import *
+from tkinter.ttk import Progressbar
+from PIL import Image, ImageTk
+
+# ALL STUFF SHOULD GO TO /dev/null HAHAHAHAHAHA
+
+
+def add_info():
+    lbl_end = Label(window, text="This software is a joke.", font=("Arial Bold", 12))
+    lbl_end.place(x=285, y=220)
+    prog.destroy()
+    lbl_end2 = Label(window, text="Never input this data to any software!", font=("Arial Bold", 12))
+    lbl_end2.place(x=285, y=240)
+
+
+def clicked():
+    lbl_thx = Label(window, text="ありがと！", font=("Arial Bold", 12))
+    lbl_thx.place(x=399, y=195)
+    prog.place(x=340, y=220)
+    prog.start()
+    prog.after(5000, add_info)
+
+
+window = Tk()
+window.title("Totally Not Malware")
+window.geometry('600x281')
+img_load = Image.open("tsukasa-hiiragi.png")
+img_rend = ImageTk.PhotoImage(img_load)
+img = Label(window, image=img_rend)
+img.image = img_rend
+img.grid(column=0, row=0)
+lbl = Label(window, text="H-hi there...", font=("Arial Bold", 12))
+lbl.place(x=400, y=0)
+lbl1 = Label(window, text="Do you th-think I could have your", font=("Arial Bold", 12))
+lbl1.place(x=325, y=20)
+lbl2 = Label(window, text="credit card information, p-please?", font=("Arial Bold", 12))
+lbl2.place(x=323, y=40)
+lbl3 = Label(window, text="Card number:", font=("Arial Bold", 12))
+lbl3.place(x=285, y=65)
+txt = Text(window, height=1, width=23)
+txt.place(x=400, y=68)
+lbl4 = Label(window, text="Expiry date:", font=("Arial Bold", 12))
+lbl4.place(x=290, y=95)
+txt1 = Text(window, height=1, width=23)
+txt1.place(x=400, y=98)
+lbl5 = Label(window, text="Security code:", font=("Arial Bold", 12))
+lbl5.place(x=280, y=125)
+txt2 = Text(window, height=1, width=23)
+txt2.place(x=400, y=128)
+prog = Progressbar(window, orient=HORIZONTAL, length=200, mode="indeterminate")
+btn = Button(window, text="Th-thanks", command=clicked, font=("Arial Bold", 12))
+btn.place(x=395, y=160)
+window.mainloop()
